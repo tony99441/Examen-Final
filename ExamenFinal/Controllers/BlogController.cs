a6cdd3cf3763ce7392ac32d862aef331ff9e5259
Anthony Briones Quiroz <n00017665@upn.pe>
2020-12-08 02:05:49 -0500

    Examen-Final

---
+++ ExamenFinal/Controllers/BlogController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExamenFinal.ConexionBD;
+using ExamenFinal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamenFinal.Controllers
+{
+    public class BlogController : Controller
+    {
+        private FinalContext _context;
+        public BlogController(FinalContext _context) {
+            this._context = _context; 
+        }
+
+        public IActionResult Index(string busqueda)
+        {
+
+            ViewBag.busqueda = busqueda;
+            ViewBag.etiquetas = _context.etiquetas.ToList();
+            return View();
+        }
+        public IActionResult _Index(string busqueda = "")
+        {
+            var notas = _context.notas.ToList();
+            var etiquetasBuscadas = new List<Etiqueta>();
+            var etiquetas = _context.etiquetas.ToList();
+            var DetalleNotasEtiquetas = _context.DetalleNotaEtiquetas.ToList();
+
+            if (!string.IsNullOrEmpty(busqueda))
+            {
+                notas = _context.notas.Where(o => o.Titulo.ToLower().Contains(busqueda) || o.Contenido.ToLower().Contains(busqueda)).ToList();
+                etiquetasBuscadas = _context.etiquetas.Where(o => o.Nombre.ToLower().Contains(busqueda)).ToList();
+            }
+
+            var indicesEtiquetas = etiquetasBuscadas.Select(o => o.Id).ToList();
+            List<DetalleNotaEtiqueta> nuevaIndices = new List<DetalleNotaEtiqueta>();
+            foreach (var item in DetalleNotasEtiquetas)
+            {
+                if (indicesEtiquetas.Contains(item.IdEtiqueta))
+                {
+                    nuevaIndices.Add(item);
+                }
+            }
+
+            var TNotas = _context.notas.ToList();
+            List<Nota> nueva = new List<Nota>();
+            var indiceDetalles = nuevaIndices.Select(o => o.IdNota).ToList();
+            foreach (var item in TNotas)
+            {
+                if (indiceDetalles.Contains(item.Id))
+                {
+                    nueva.Add(item);
+                }
+            }
+
+            var listaFinal = nueva.Union(notas);
+            Dictionary<int, string> resumen = new Dictionary<int, string>();
+            var contenido = "";
+            foreach (var item in listaFinal)
+            {
+                if (item.Contenido.Length < 50)
+                {
+                    contenido = item.Contenido;
+                }
+                else
+                {
+                    contenido = item.Contenido.Substring(0, 50);
+                }
+
+                resumen.Add(item.Id, contenido);
+            }
+
+            ViewBag.resumen = resumen;
+            ViewBag.etiquetas = _context.etiquetas.ToList();
+            ViewBag.detalles = DetalleNotasEtiquetas;
+            return View(listaFinal);
+
+        }
+
+        public IActionResult CrearNota()
+        {
+
+            return View();
+        }
+        public IActionResult CrearNotaA(Nota nota)
+        {
+            nota.UltimaModificacion = DateTime.Now;
+            _context.notas.Add(nota);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult EliminarNotra(int IdNota)
+        {
+            var nota = _context.notas.Where(o => o.Id == IdNota).FirstOrDefault();
+            _context.notas.Remove(nota);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult EditarNota(int IdNota)
+        {
+            ViewBag.Nota = _context.notas.Where(o => o.Id == IdNota).FirstOrDefault();
+            return View();
+        }
+        public IActionResult EditarNotaA(Nota nota)
+        {
+            var nota2 = _context.notas.Where(o => o.Id == nota.Id).FirstOrDefault();
+            nota2.Contenido = nota.Contenido;
+            nota2.Titulo = nota.Titulo;
+            nota2.UltimaModificacion = DateTime.Now;
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult DetalleNota(int IdNota)
+        {
+            var nota = _context.notas.Where(o => o.Id == IdNota).FirstOrDefault();
+            ViewBag.Nota = nota;
+            return View();
+        }
+    }
+}
